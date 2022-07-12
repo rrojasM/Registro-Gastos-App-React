ddd49f9521cba4acf7314a72883571b6c404c1ab
Rufino Rojas <rojasru@globalhitss.com>
2022-07-12 12:43:08 -0500

    add dependencies and google fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Work Sans:400,500,700', 'sans-serif']
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
