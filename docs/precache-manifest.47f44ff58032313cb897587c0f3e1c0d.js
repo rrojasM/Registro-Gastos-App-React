536944978b56a09d8ffdd60504691c198baeee74
Rufino Rojas <ruf_rm@hotmail.com>
2022-08-15 21:19:43 -0500

    build example

---
+++ docs/precache-manifest.47f44ff58032313cb897587c0f3e1c0d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d087c44033846e023a3",
+    "url": "/static/css/main.daaea7d2.chunk.css"
+  },
+  {
+    "revision": "5d087c44033846e023a3",
+    "url": "/static/js/main.5d087c44.chunk.js"
+  },
+  {
+    "revision": "0efa27dd25396d00191c",
+    "url": "/static/css/1.edf69654.chunk.css"
+  },
+  {
+    "revision": "0efa27dd25396d00191c",
+    "url": "/static/js/1.0efa27dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1895dd62d8a74f20330df79737112c8b",
+    "url": "/static/media/log-out.1895dd62.svg"
+  },
+  {
+    "revision": "925bd082901ff89a3355b7c8075c1ac0",
+    "url": "/static/media/plus.925bd082.svg"
+  },
+  {
+    "revision": "aa3ea191970b9704a491f42a768cb75f",
+    "url": "/static/media/cat_comida.aa3ea191.svg"
+  },
+  {
+    "revision": "465c3bb0af1e73b053a83c5013622fe5",
+    "url": "/static/media/cat_compras.465c3bb0.svg"
+  },
+  {
+    "revision": "f6d76f9fe907d6b1868f747ba553d659",
+    "url": "/static/media/cat_cuentas-y-pagos.f6d76f9f.svg"
+  },
+  {
+    "revision": "8a2ff2ebc8eccee1a6aae829c843a87f",
+    "url": "/static/media/cat_diversion.8a2ff2eb.svg"
+  },
+  {
+    "revision": "f6f5cc435e7dd6dcd8bbada7dacdba9c",
+    "url": "/static/media/cat_hogar.f6f5cc43.svg"
+  },
+  {
+    "revision": "70de1ae99121fa508adee683f00a03dd",
+    "url": "/static/media/cat_ropa.70de1ae9.svg"
+  },
+  {
+    "revision": "7bf18ed4c27645eb5440d79b8901a1d9",
+    "url": "/static/media/cat_salud-e-higiene.7bf18ed4.svg"
+  },
+  {
+    "revision": "4956de8578d86a7a0e39112f2a3a96d1",
+    "url": "/static/media/cat_transporte.4956de85.svg"
+  },
+  {
+    "revision": "07f27d1d8fdd8acb6e364f17ebc73ef2",
+    "url": "/static/media/down.07f27d1d.svg"
+  },
+  {
+    "revision": "998099119a6d8ea99a891b6afc00c9df",
+    "url": "/static/media/login.99809911.svg"
+  },
+  {
+    "revision": "889f7b44e38668919ce059ac1d1d8f9d",
+    "url": "/static/media/registro.889f7b44.svg"
+  },
+  {
+    "revision": "80272c77e843e7b85477e9cdc1750ef1",
+    "url": "/static/media/flecha.80272c77.svg"
+  },
+  {
+    "revision": "4b8550d1b770519e5a3951cfe461a9ff",
+    "url": "/static/media/editar.4b8550d1.svg"
+  },
+  {
+    "revision": "62eefd11215424ec653ab194330af840",
+    "url": "/static/media/borrar.62eefd11.svg"
+  },
+  {
+    "revision": "d763fc1260c66329c044176935361adb",
+    "url": "/static/media/puntos.d763fc12.svg"
+  },
+  {
+    "revision": "2aee21875d92167f8a10190ec130ce87",
+    "url": "/index.html"
+  }
+];
